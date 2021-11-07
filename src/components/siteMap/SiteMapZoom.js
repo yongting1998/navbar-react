8f258504976fbb8ce15dd43a57e68fde46305fb5
Germaine Wong <germaine@pigeonlab.com>
2021-11-07 19:32:39 +0800

    zoom to center and fix button zooming

--- src/components/siteMap/SiteMapZoom.js
+++ src/components/siteMap/SiteMapZoom.js
@@ -1,18 +1,9 @@
 import React, { createRef, useEffect, useState, useRef } from "react";
 
-
-var click;
-
 const SiteMapZoom = () => {
   const canvasRef = createRef();
   const contextRef = useRef(null);
 
-  /**Record whether the mouse is pressed*/
-  const [mouseDowmFlag, setMouseDowmFlag] = useState(false);
-  /**Record the coordinates of the mouse down*/
-  const [mouseDowmPos, setMouseDowmPos] = useState({ x: 0, y: 0 });
-  /**Record the distance before this translation*/
-  const [offsetDis, setOffsetDis] = useState({ left: 0, top: 0 });
   /**Picture node*/
   const [imgElement, setImgElement] = useState(new Image());
   /**Display image size*/
@@ -22,6 +13,10 @@ const SiteMapZoom = () => {
   const [pointsList, setPointsList] = useState([]);
   const [points, setPoints] = useState([]);
   const [startPoint, setStartPoint] = useState({});
+
+  const [scaleFactor, setScaleFactor] = useState(1.0);
+  const [pan, setPan] = useState({ x: 0, y: 0 });
+
 
   useEffect(() => {
     /**Initializes a picture node object*/
@@ -33,7 +28,7 @@ const SiteMapZoom = () => {
       const height = background.naturalHeight;
       canvas.width = width;
       canvas.height = height;
-      canvas.style.width = width+ "px";
+      canvas.style.width = width + "px";
       canvas.style.height = height + "px";
       canvas.style.border = "1px solid black";
 
@@ -50,112 +45,50 @@ const SiteMapZoom = () => {
       setSize({ width, height });
       setImgElement(background);
     };
-    //img.height = HEIGHT;
-    //img.src = imgInfo.src;
   }, []);
 
-  /**Convert browser coordinate system to canvas coordinate system*/
-  const windowToCanvas = (canvas, x, y) => {
-    var canvasBox = canvas.getBoundingClientRect();
-    return {
-      x: (x - canvasBox.left) * (canvas.width / canvasBox.width), // / Zoom when the size of the canvas element is inconsistent with the size of the drawing surface
-      y: (y - canvasBox.top) * (canvas.height / canvasBox.height),
-    };
-  };
-
-  /**Picture translation*/
-  const handleMouseDown = (event) => {
-    event.stopPropagation();
-    event.preventDefault(); //  Block browser default behavior, drag to open picture
-    const { clientX, clientY } = event;
-    //Relative to canvas coordinates
+  const zoomPan = () => {
     const canvas = canvasRef.current;
-    const pos = windowToCanvas(canvas, clientX, clientY);
-    canvas.style.cursor = "move";
-    setMouseDowmFlag(true); //  The control only performs MouseMove when the mouse is pressed
-    setMouseDowmPos({
-      x: pos.x,
-      y: pos.y,
-    });
-  };
-
-  const handleMouseMove = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    if (!mouseDowmFlag) return;
-    const { clientX, clientY } = event;
-    const canvas = canvasRef.current;
-    //Relative to canvas coordinates
-    const pos = windowToCanvas(canvas, clientX, clientY);
-    //Offset
-    const diffX = pos.x - mouseDowmPos.x;
-    const diffY = pos.y - mouseDowmPos.y;
-    if (diffX === 0 && diffY === 0) return;
-    //Coordinate positioning = last positioning + offset
-    const offsetX = parseInt(`${diffX + offsetDis.left}`, 10);
-    const offsetY = parseInt(`${diffY + offsetDis.top}`, 10);
-    //Pan picture
     const ctx = canvas.getContext("2d");
-    //Empty canvas
-    ctx.clearRect(0, 0, size.width, size.height);
-    //Draw a picture
-    ctx.drawImage(imgElement, offsetX, offsetY, size.width, size.height);
 
-    //Update pressed coordinates
-    setMouseDowmPos({
-      x: pos.x,
-      y: pos.y,
-    });
-    //Update last coordinates
-    setOffsetDis({
-      left: offsetX,
-      top: offsetY,
-    });
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, size.width, size.height);
+    ctx.save();
+    ctx.translate(size.width / 2, size.height / 2);
+    ctx.scale(scaleFactor, scaleFactor);
+    ctx.translate(pan.x, pan.y);
+    ctx.drawImage(
+      imgElement,
+      size.width / -2,
+      size.height / -2,
+      size.width,
+      size.height
+    );
+    ctx.restore();
   };
 
-  const handleMouseUp = (event) => {
+  const handlePan = (event) => {
     event.stopPropagation();
     event.preventDefault();
-    const canvas = canvasRef.current;
-    canvas.style.cursor = "default";
-    setMouseDowmFlag(false);
+    if (event.buttons === 1) {
+      pan.x += event.movementX;
+      pan.y += event.movementY;
+
+      zoomPan();
+    }
   };
 
   /**Scrolling*/
-  const handleWheelImage = (event) => {
-    click = event.target.id;
+  const handleZoom = (event) => {
     event.stopPropagation();
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    if(event.deltaY > 0 || click == "decrease") {
-      var bigger = -1;
+    if (event.deltaY > 0) {
+      setScaleFactor(scaleFactor * 1.1);
     } else {
-      var bigger = 1;
+      setScaleFactor(scaleFactor / 1.1);
     }
-    //Up is negative and down is positive
-    // const bigger = event.deltaY > 0 ? -1 : 1;
-    //Magnification
-    const enlargeRate = 1.2;
-    //Scale down
-    const shrinkRate = 0.8;
-
-    //Scaling
-    const { height: initHeight, naturalHeight, naturalWidth } = imgElement;
-    const imgScale = initHeight / naturalHeight;
-
-    const rate = bigger > 0 ? enlargeRate : shrinkRate;
-    const width = size.width * rate;
-    const height = size.height * rate;
-
-    //Empty canvas
-    ctx.clearRect(0, 0, size.width, size.height);
-    ctx.drawImage(imgElement, offsetDis.left, offsetDis.top, width, height);
-
-    setSize({ width, height });
-    return false;
+    zoomPan();
   };
 
-  
   const startDrawing = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
     if (points.length === 0) {
@@ -188,15 +121,17 @@ const SiteMapZoom = () => {
     }
   }
 
+  useEffect(() => {
+    zoomPan();
+  }, [scaleFactor]);
+
   return (
     <div style={{ display: "flex" }}>
       <canvas
         ref={canvasRef}
-        onWheel={handleWheelImage}
-        onMouseDown={handleMouseDown}
-        onDoubleClick={startDrawing}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
+        onWheel={handleZoom}
+        onMouseDown={startDrawing}
+        onMouseMove={handlePan}
       ></canvas>
 
       <div
@@ -207,11 +142,14 @@ const SiteMapZoom = () => {
           margin: 5,
         }}
       >
-        <button className="" onClick={handleWheelImage}>
+        <button name="btn-zoomin" className=""  onClick={() => setScaleFactor(scaleFactor * 1.1)}>
           <span>&#43;</span>
         </button>
-        <button id="decrease" className="" onClick={handleWheelImage}>
+        <button name="btn-zoomout" className="" onClick={() => setScaleFactor(scaleFactor / 1.1)}>
           <span>&#8722;</span>
+        </button>
+        <button name="btn-resetZoom" className="" onClick={() => setScaleFactor(1.0)}>
+          <span style={{ fontSize: "14px" }}>Reset</span>
         </button>
       </div>
     </div>
